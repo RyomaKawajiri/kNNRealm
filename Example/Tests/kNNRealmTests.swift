2d8d9f2a075a9fe12781c935639a5a14ad3a4663
Kawajiri Ryoma <ryoma.edison@gmail.com>
2015-06-26 11:23:27 +0900

    create a test file

---
+++ Example/Tests/kNNRealmTests.swift
@@ -0,0 +1,30 @@
+//
+//  kNNRealmTests.swift
+//  kNNRealm
+//
+//  Created by Kawajiri Ryoma on 6/26/15.
+//  Copyright (c) 2015 Kawajiri Ryoma. All rights reserved.
+//
+
+import Quick
+import Nimble
+import kNNRealm
+
+class GeneratedDataSetSpec: QuickSpec {
+  override func spec() {
+    beforeSuite {
+      // generate test data
+    }
+
+    beforeEach {
+      // set test data to realm
+    }
+
+    afterEach {
+      // clear all realm
+    }
+
+    describe("simple") {
+    }
+  }
+}
